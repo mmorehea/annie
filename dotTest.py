04bd11c5356f1e5ea307dcc5b55ad8928f504fc7
Michael Morehead <mmorehea@mix.wvu.edu>
2016-08-11 12:41:19 -0400

    wave prop works

--- dotTest.py
+++ dotTest.py
@@ -10,6 +10,7 @@ def recSearch(pixel, img, color):
     found = [pixel]
 
     while len(front) > 0:
+        #print front
         fronty = front
         front = []
         for each in fronty:
@@ -18,15 +19,15 @@ def recSearch(pixel, img, color):
             searchPixels = [[pixel[0]+1, pixel[1]], [pixel[0]-1, pixel[1]], [pixel[0], pixel[1]+1], [pixel[0], pixel[1]-1]]
 
             for neighbor in searchPixels:
-                code.interact(local=locals())
-                if img[neighbor] == color:
-                    front.append(each)
-                    found.append(each)
-
+                #code.interact(local=locals())
+                if img[neighbor[0], neighbor[1]] == color and neighbor not in front and neighbor not in found:
+                    front.append([neighbor[0], neighbor[1]])
+                    found.append([neighbor[0], neighbor[1]])
+        #code.interact(local=locals())
     return found
 
 
-img1 = cv2.imread("dot.png", -1)
+img1 = cv2.imread("dot.tif", -1)
 print img1
 print np.amin(img1)
 s = recSearch([16,16], img1, 0)
