41577baf17d05291bc644e8a0b18356e66664aad
Michael Morehead <mmorehea@mix.wvu.edu>
2016-08-11 12:41:19 -0400

    wave prop works

--- auto.py
+++ auto.py
@@ -7,9 +7,10 @@ import cv2
 import glob
 import code
 import numpy as np
+from timeit import default_timer as timer
 # initialize the list of reference points and boolean indicating
 # whether cropping is being performed or not
-
+np.set_printoptions(threshold=np.inf)
 #def findNextLabel(img1, img2):
 def findBB(dlist):
 	xs = dlist[0]
@@ -28,7 +29,6 @@ def buildColorMap(img):
 			else:
 				counter += 1
 				colorMap[counter] = each
-
 	#print colorMap
 	return colorMap
 
@@ -36,28 +36,43 @@ def waterShedSearch(searchSpace, img, color):
 	viablePixels = np.where(searchSpace == color)
 	startX = viablePixels[0][0]
 	startY = viablePixels[1][0]
+	return recSearch([startX, startY], img, color)
 
 
-def recSearch(pixel, img, color, listOfPixels, listOfPixelsToSearch):
+def recSearch(pixel, img, color):
 
-	if pixel not in listOfPixels:
-		listOfPixels.append(pixel)
-		listOfPixelsToSearch.append(pixel)
+	front = [pixel]
+	found = [pixel]
 
-	while len(listOfPixelsToSearch) > 0:
-		pixel = listOfPixelsToSearch.pop()
-		searchPixels = [[pixel[0]+1, pixel[1]], [pixel[0]-1, pixel[1]], [pixel[0], pixel[1]+1], [pixel[0], pixel[1]-1]]
-		count = 0
-		for each in searchPixels:
-			if img[each] == color and each not in listOfPixels:
-				listOfPixels.append(each)
-				listOfPixelsToSearch.append(each)
-			else:
-				count += 1
-		if count == 4:
-			continue
+	foundGrid = np.zeros((img.shape[0], img.shape[1]))
+	foundGrid[pixel[0], pixel[1]] = 1
+	counter = 0
+	while len(front) > 0:
+		fronty = front
+		front = []
 
-		listOfPixelsToSearch.remove(pixel)
+		for each in fronty:
+			pixel = each
+			searchPixels = [[pixel[0]+1, pixel[1]], [pixel[0]-1, pixel[1]], [pixel[0], pixel[1]+1], [pixel[0], pixel[1]-1]]
+			#code.interact(local=locals())
+			for neighbor in searchPixels:
+				if neighbor[0] not in range(img.shape[0]) or neighbor[1] not in range(img.shape[1]):
+					#print "hit border, skipping"
+					continue
+				#code.interact(local=locals())
+				if img[neighbor[0], neighbor[1]] == color and foundGrid[neighbor[0], neighbor[1]] == 0 and neighbor not in front:
+					#code.interact(local=locals())
+					front.append([neighbor[0], neighbor[1]])
+					foundGrid[neighbor[0], neighbor[1]] = 1
+					counter = counter + 1
+					#found.append([neighbor[0], neighbor[1]])
+
+	found = np.where(foundGrid == 1)
+	#
+	found = zip(found[0],found[1])
+
+	#code.interact(local=locals())
+	return found
 
 
 
@@ -73,20 +88,33 @@ args = vars(ap.parse_args())
 
 # load the image, clone it, and setup the mouse callback function
 list_of_images = sorted(glob.glob(args["dir"] +'*'))
+start = timer()
 
 for imageCount in xrange(len(list_of_images) - 1):
+	#if imageCount % 100 == 0:
+	#print imageCount
 	imgPath1 = list_of_images[imageCount]
 	imgPath2 = list_of_images[imageCount+1]
 
 	img1 = cv2.imread(imgPath1, -1)
 	img2 = cv2.imread(imgPath2, -1)
-
+	newImg = img2
+	if img1.shape != img2.shape:
+		print "THE IMAGES ARE THE WRONG SIZE"
+		break
 	colorMap = buildColorMap(img1)
-
+	numberOfColors = len(colorMap.values()[1:])
+	colorCount = 1
 	for color in colorMap.values()[1:]:
+		if colorCount % 100 == 0:
+			print str(colorCount) + ' / ' + str(numberOfColors)
+			end = timer()
+			print(end - start)
+		colorCount += 1
+		print "Color is " + str(color)
 		firstshape = np.where(img1 == color)
 		minX, maxX, minY, maxY = findBB(firstshape)
-
+		#code.interact(local=locals())
 		searchSpace = img2[minX:maxX, minY:maxY]
 		if searchSpace.size < 17:
 			continue
@@ -98,15 +126,22 @@ for imageCount in xrange(len(list_of_images) - 1):
 		else:
 			counts = np.bincount(searchSpaceFlat)
 			mode = np.argmax(counts)
-
+			print "Mode is " + str(mode)
+			if mode == 0:
+				print "woah"
 			pixelsMatch = np.where(img2[minX:maxX, minY:maxY] == mode)
 			newXs = pixelsMatch[0] + minX
 			newYs = pixelsMatch[1] + minY
-			pixelsMatch = [[newXs], [newYs]]
 			#code.interact(local=locals())
-			img2[pixelsMatch] = color
+			pixelsMatch = recSearch([newXs[0], newYs[0]], img2, mode)
+			print len(pixelsMatch)
+			#code.interact(local=locals())
+			try:
+				newImg[pixelsMatch] = color
+			except IndexError:
+				continue
 
-cv2.imwrite("test.tiff", img2)
+cv2.imwrite("test.tiff", newImg)
 
 
 #while True:
