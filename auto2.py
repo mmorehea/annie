543bc8b241abe28683c3681cd26f8f62084a609e
Michael Morehead <mmorehea@mix.wvu.edu>
2016-12-08 18:43:53 -0500

    a

--- auto2.py
+++ auto2.py
@@ -53,6 +53,23 @@ def findCentroid(listofpixels):
 		centroid = (0,0)
 	return centroid
 
+def getMeasurements(blob, shape):
+	img = np.zeros(shape, np.uint16)
+	img[zip(*blob)] = 1
+	per = []
+	for p in blob:
+		x = p[0]
+		y = p[1]
+		q = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
+
+		edgePoint = False
+		for each in q:
+			if img[each] == 0:
+				edgePoint = True
+		if edgePoint:
+			per.append(p)
+	return len(blob), len(per)
+
 def testOverlap(setofpixels1, setofpixels2):
 
 	set_intersection = setofpixels1 & setofpixels2
@@ -99,6 +116,15 @@ def waterShed(blob, shape):
 
 	return subBlobs
 
+def shapeMerge(shape1, shape2):
+	# from http://stackoverflow.com/questions/14730340/find-the-average-vector-shape
+	if len(shape2) > len(shape1):
+		shape1, shape2 = shape2, shape1
+
+	for point in shape1:
+		near =
+
+
 def display(blob):
 
 	img = np.zeros(shape, np.uint16)
@@ -127,278 +153,303 @@ build_resultStack = True
 load_stack_from_pickle_file = False
 indices_of_slices_to_be_removed = []
 ################################################################################
+def main():
+	dirr = sys.argv[1]
 
-dirr = sys.argv[1]
+	list_of_image_paths = sorted(glob.glob(dirr +'*'))
 
-list_of_image_paths = sorted(glob.glob(dirr +'*'))
+	list_of_image_paths = [i for j, i, in enumerate(list_of_image_paths) if j not in indices_of_slices_to_be_removed]
 
-list_of_image_paths = [i for j, i, in enumerate(list_of_image_paths) if j not in indices_of_slices_to_be_removed]
+	shape = cv2.imread(list_of_image_paths[0],-1).shape
 
-shape = cv2.imread(list_of_image_paths[0],-1).shape
+	start = timer()
 
-start = timer()
+	if trace_objects:
 
-if trace_objects:
+		chainLengths = []
 
-	chainLengths = []
+		images = []
+		for i, path in enumerate(list_of_image_paths):
+			im = cv2.imread(path, -1)
+			images.append(im)
+		print 'Loaded ' + str(len(images)) + ' images.'
 
-	images = []
-	for i, path in enumerate(list_of_image_paths):
-		im = cv2.imread(path, -1)
-		images.append(im)
-	print 'Loaded ' + str(len(images)) + ' images.'
+		imageArray = np.dstack(images)
 
-	imageArray = np.dstack(images)
+		colorList = []
+		for z in xrange(imageArray.shape[2]):
+			colorList.extend([c for c in np.unique(imageArray[:,:,z]) if c!=0])
+		colorList = list(set(colorList))
 
-	colorList = []
-	for z in xrange(imageArray.shape[2]):
-		colorList.extend([c for c in np.unique(imageArray[:,:,z]) if c!=0])
-	colorList = list(set(colorList))
+		objectCount = -1
+		for z in xrange(imageArray.shape[2]):
+			###Testing###
+			if z != 0:
+				continue
+			#############
+			image = imageArray[:,:,z]
 
-	objectCount = -1
-	for z in xrange(imageArray.shape[2]):
-		###Testing###
-		if z != 0:
-			continue
-		#############
-		image = imageArray[:,:,z]
+			colorVals = [c for c in np.unique(image) if c!=0]
+			###Testing###
+			colorVals = [5724]
+			# 6228, 5724, 7287
+			#############
 
-		colorVals = [c for c in np.unique(image) if c!=0]
-		###Testing###
-		colorVals = [7287]
-		# 6228, 5724, 7287
-		#############
+			blobs = []
+			for color in colorVals:
+				wblob = np.where(image==color)
+				blob = zip(wblob[0], wblob[1])
+				blobs.append(blob)
 
-		blobs = []
-		for color in colorVals:
-			wblob = np.where(image==color)
-			blob = zip(wblob[0], wblob[1])
-			blobs.append(blob)
+			blobs = sorted(blobs, key=len)
 
-		blobs = sorted(blobs, key=len)
+			###Testing###
+			# testblob = blobs[94]
+			# blobs = [testblob]
+			#############
 
-		###Testing###
-		# testblob = blobs[94]
-		# blobs = [testblob]
-		#############
+			for i, startBlob in enumerate(blobs):
+				# xs = []
+				# ys = []
+				# xslopes = []
+				# yslopes = []
+				measurementsList = []
+				# print str(i+1) + '/' + str(len(blobs))
 
-		for i, startBlob in enumerate(blobs):
-			xs = []
-			ys = []
-			xslopes = []
-			yslopes = []
-			# print str(i+1) + '/' + str(len(blobs))
+				box, dimensions = findBBDimensions(startBlob)
 
-			box, dimensions = findBBDimensions(startBlob)
+				color1 = image[startBlob[0]]
+				ogcolor = color1
 
-			color1 = image[startBlob[0]]
-			ogcolor = color1
+				centroid1 = findCentroid(startBlob)
 
-			centroid1 = findCentroid(startBlob)
+				startZ = z
 
-			startZ = z
+				process = [startBlob]
 
-			process = [startBlob]
+				image[zip(*startBlob)] = 0
 
-			image[zip(*startBlob)] = 0
+				zspace = 0
+				d = 0
+				terminate = False
+				currentBlob = startBlob
 
-			zspace = 0
-			d = 0
-			terminate = False
-			currentBlob = startBlob
+				while terminate == False:
+					measurementsList.append(getMeasurements(currentBlob, shape))
 
-			while terminate == False:
+					zspace += 1
+					blobsfound = []
 
-				zspace += 1
-				blobsfound = []
+					try:
+						image2 = imageArray[:,:,z+zspace]
+					except:
+						terminate = True
+						s = '0'
+						continue
 
-				try:
-					image2 = imageArray[:,:,z+zspace]
-				except:
-					terminate = True
-					s = '0'
+					window = image2[box[0]:box[1], box[2]:box[3]]
+
+					organicWindow = image2[zip(*currentBlob)]
+					frequency = collections.Counter(organicWindow).most_common()
+
+
+					if frequency[0][0] == 0 and len(frequency) == 1:
+						if d > 10:
+							terminate = True
+							while d > 0:
+								del process[-1]
+								d -= 1
+							continue
+						else:
+							# xslopes.append(dzdx)
+							# yslopes.append(dzdy)
+							# xs.append(x)
+							# ys.append(y)
+							process.append([])
+							d += 1
+							continue
+
+					for each in frequency:
+						if each[0] == 0:
+							continue
+						clr, freq = each
+						break
+
+					q = np.where(image2 == clr)
+					blob2 = zip(q[0],q[1])
+					centroid2 = findCentroid(blob2)
+
+					overlap = testOverlap(set(currentBlob), set(blob2))
+					coverage = freq / float(len(organicWindow))
+
+					# if zspace: >= 886:
+					# 	img = np.zeros(shape,uint16)
+					# 	img[zip(*averageBlob)] = 99999
+					# 	code.interact(local=locals())
+
+					if coverage > 0.75:
+						if overlap > 0.75:
+							blobsfound.append(blob2)
+						elif overlap > 0.5 and d > 3:
+							blobsfound.append(blob2)
+						elif overlap > 0.1:
+							subBlobs = waterShed(blob2, shape)
+							subBlobs, overlapVals = orderByPercentOverlap(subBlobs, currentBlob)
+							for i, sb in enumerate(subBlobs):
+								if overlapVals[i] > 0.1:
+									blobsfound.append(sb)
+							if len(blobsfound) == 0:
+								try:
+									blobsfound.append(subBlobs[0])
+								except:
+									blobsfound.append(blob2)
+						else:
+							# xslopes.append(dzdx)
+							# yslopes.append(dzdy)
+							# xs.append(x)
+							# ys.append(y)
+							process.append([])
+							continue
+					else:
+						blobsfound.append(blob2)
+
+					# dzdx = centroid2[0] - centroid1[0]
+					# dzdy = centroid2[1] - centroid1[1]
+					# x = centroid1[0]
+					# y = centroid1[1]
+
+					# xslopes.append(dzdx)
+					# yslopes.append(dzdy)
+					# xs.append(x)
+					# ys.append(y)
+
+					if terminate == False:
+
+						newBlob = []
+						for b in blobsfound:
+							newBlob += b
+
+						averageBlob = shapeMerge(currentBlob, newBlob)
+
+						currentBlob = newBlob
+
+						#Probably need to do the stuff below when I terminate as well
+						color1 = image2[currentBlob[0]]
+
+						image2[zip(*currentBlob)] = 0
+
+						process.append(currentBlob)
+
+						box,dimensions = findBBDimensions(currentBlob)
+
+						d = 0
+
+						centroid1 = findCentroid(currentBlob)
+
+
+
+				if len(process) > minimum_process_length:
+					# fig = plt.figure()
+					# ax = fig.gca(projection='3d')
+					# xs = np.array(xs)
+					# ys = np.array(ys)
+					# zs = np.array(range(imageArray.shape[2]-1)[::-1])
+					#
+					#
+					# ax.plot(xs,ys,zs)
+
+					# plt.plot(xs,'ro')
+					# plt.show()
+					# plt.plot(ys,'ro')
+					# plt.show()
+					# plt.plot(xslopes,'ro')
+					# plt.show()
+					# plt.plot(yslopes,'ro')
+					# plt.show()
+					# code.interact(local=locals())
+					objectCount += 1
+
+					color = colorList[objectCount]
+
+					print '\n'
+					print objectCount
+					end = timer()
+					print(end - start)
+					print '\n'
+
+					chainLengths.append((objectCount, color, len(process)))
+					pickle.dump((startZ, process, color), open(write_pickles_to + str(objectCount) + '.p', 'wb'))
+
+		print 'Number of chains: ' + str(len(chainLengths))
+		print 'Average chain length: ' + str(sum([x[0] for x in chainLengths])/len(chainLengths))
+		# print s
+
+		if os.path.exists('summary.txt'):
+			os.remove('summary.txt')
+
+		chainLengths = sorted(chainLengths)[::-1]
+
+		with open('summary.txt','w') as f:
+			for i,each in enumerate(chainLengths):
+				f.write(str(chainLengths[i][0]) + ' ' + str(chainLengths[i][1]) + ' ' + str(chainLengths[i][2]) + '\n')
+
+
+	if build_resultStack:
+
+		picklePaths = sorted(glob.glob(write_pickles_to + '*.p'))
+
+		if load_stack_from_pickle_file:
+			resultArray, startO = pickle.load(open('resultArraySave.p', 'rb'))
+		else:
+			resultArray = np.zeros((shape[0], shape[1], len(list_of_image_paths)), np.uint16)
+			startO = 0
+
+
+		for o, path in enumerate(picklePaths):
+			if o < startO:
+				continue
+
+			startZ, process, color = pickle.load(open(path, 'rb'))
+
+			for z in xrange(resultArray.shape[2]):
+				img = resultArray[:,:,z]
+
+				if z < startZ:
 					continue
 
-				window = image2[box[0]:box[1], box[2]:box[3]]
+				if z >= startZ + len(process):
+					continue
 
-				organicWindow = image2[zip(*currentBlob)]
-				frequency = collections.Counter(organicWindow).most_common()
+				img[zip(*process[z - startZ])] = color
 
-				# if z+zspace >= 74:
-				# 	img = np.zeros(shape, np.uint16)
-				# 	img[zip(*currentBlob)] = 99999
-				# 	code.interact(local=locals())
+			pickle.dump((resultArray, o), open('resultArraySave.p,','wb'))
 
-
-				if frequency[0][0] == 0 and len(frequency) == 1:
-					if d > 10:
-						terminate = True
-						while d > 0:
-							del process[-1]
-							d -= 1
-						continue
-					else:
-						xslopes.append(dzdx)
-						yslopes.append(dzdy)
-						xs.append(x)
-						ys.append(y)
-						process.append([])
-						d += 1
-						continue
-
-				for each in frequency:
-					if each[0] == 0:
-						continue
-					clr, freq = each
-					break
-
-				q = np.where(image2 == clr)
-				blob2 = zip(q[0],q[1])
-				centroid2 = findCentroid(blob2)
-
-				overlap = testOverlap(set(currentBlob), set(blob2))
-				coverage = freq / float(len(organicWindow))
-
-				if coverage > 0.75:
-					if overlap > 0.75:
-						blobsfound.append(blob2)
-					elif overlap > 0.5 and d > 3:
-						blobsfound.append(blob2)
-					elif overlap > 0.1:
-						subBlobs = waterShed(blob2, shape)
-						subBlobs, overlapVals = orderByPercentOverlap(subBlobs, currentBlob)
-						for i, sb in enumerate(subBlobs):
-							if overlapVals[i] > 0.1:
-								blobsfound.append(sb)
-						if len(blobsfound) == 0:
-							try:
-								blobsfound.append(subBlobs[0])
-							except:
-								blobsfound.append(blob2)
-					else:
-						xslopes.append(dzdx)
-						yslopes.append(dzdy)
-						xs.append(x)
-						ys.append(y)
-						process.append([])
-						continue
-				else:
-					blobsfound.append(blob2)
-
-				dzdx = centroid2[0] - centroid1[0]
-				dzdy = centroid2[1] - centroid1[1]
-				x = centroid1[0]
-				y = centroid1[1]
-
-				xslopes.append(dzdx)
-				yslopes.append(dzdy)
-				xs.append(x)
-				ys.append(y)
-
-				if terminate == False:
-
-					currentBlob = []
-					for b in blobsfound:
-						currentBlob += b
-
-					#Probably need to do the stuff below when I terminate as well
-					color1 = image2[currentBlob[0]]
-
-					image2[zip(*currentBlob)] = 0
-
-					process.append(currentBlob)
-
-					box,dimensions = findBBDimensions(currentBlob)
-
-					d = 0
-
-					centroid1 = findCentroid(currentBlob)
-
-
-
-			if len(process) > minimum_process_length:
-				fig = plt.figure()
-				ax = fig.gca(projection='3d')
-				xs = np.array(xs)
-				ys = np.array(ys)
-				zs = np.array(range(imageArray.shape[2]-1)[::-1])
-
-
-				ax.plot(xs,ys,zs)
-
-				# plt.plot(xs,'ro')
-				# plt.show()
-				# plt.plot(ys,'ro')
-				# plt.show()
-				# plt.plot(xslopes,'ro')
-				# plt.show()
-				# plt.plot(yslopes,'ro')
-				# plt.show()
-				# code.interact(local=locals())
-				objectCount += 1
-
-				color = colorList[objectCount]
-
-				print '\n'
-				print objectCount
-				end = timer()
-				print(end - start)
-				print '\n'
-
-				chainLengths.append((objectCount, color, len(process)))
-				pickle.dump((startZ, process, color), open(write_pickles_to + str(objectCount) + '.p', 'wb'))
-
-	print 'Number of chains: ' + str(len(chainLengths))
-	print 'Average chain length: ' + str(sum([x[0] for x in chainLengths])/len(chainLengths))
-	# print s
-
-	if os.path.exists('summary.txt'):
-		os.remove('summary.txt')
-
-	chainLengths = sorted(chainLengths)[::-1]
-
-	with open('summary.txt','w') as f:
-		for i,each in enumerate(chainLengths):
-			f.write(str(chainLengths[i][0]) + ' ' + str(chainLengths[i][1]) + ' ' + str(chainLengths[i][2]) + '\n')
-
-
-if build_resultStack:
-
-	picklePaths = sorted(glob.glob(write_pickles_to + '*.p'))
-
-	if load_stack_from_pickle_file:
-		resultArray, startO = pickle.load(open('resultArraySave.p', 'rb'))
-	else:
-		resultArray = np.zeros((shape[0], shape[1], len(list_of_image_paths)), np.uint16)
-		startO = 0
-
-
-	for o, path in enumerate(picklePaths):
-		if o < startO:
-			continue
-
-		startZ, process, color = pickle.load(open(path, 'rb'))
+			print '\n'
+			print 'Built object ' + str(o+1) + '/' + str(len(picklePaths))
+			end = timer()
+			print(end - start)
+			print '\n'
 
 		for z in xrange(resultArray.shape[2]):
-			img = resultArray[:,:,z]
+			image = resultArray[:,:,z]
+			cv2.imwrite(write_images_to + list_of_image_paths[z][list_of_image_paths[z].index('/')+1:], image)
 
-			if z < startZ:
-				continue
+		diffarray = [(measurement[1]**2)/(4*3.1415926) - measurement[0] for measurement in measurementsList]
 
-			if z >= startZ + len(process):
-				continue
+		ratarray = [float(measurement[1])/measurement[0] for measurement in measurementsList]
 
-			img[zip(*process[z - startZ])] = color
+		plt.figure(1)
+		plt.subplot(211)
+		plt.plot(zip(*measurementsList)[0])
+		plt.subplot(212)
+		plt.plot(zip(*measurementsList)[1])
+		plt.figure(2)
+		plt.plot(diffarray)
+		plt.figure(3)
+		plt.plot(ratarray)
+		plt.show()
+		code.interact(local=locals())
 
-		pickle.dump((resultArray, o), open('resultArraySave.p,','wb'))
 
-		print '\n'
-		print 'Built object ' + str(o+1) + '/' + str(len(picklePaths))
-		end = timer()
-		print(end - start)
-		print '\n'
-
-	for z in xrange(resultArray.shape[2]):
-		image = resultArray[:,:,z]
-		cv2.imwrite(write_images_to + list_of_image_paths[z][list_of_image_paths[z].index('/')+1:], image)
-	plt.show()
+if __name__ == "__main__":
+	main()
